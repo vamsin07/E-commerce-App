077799e642d9595675376737c79406d0c6e74c3b
Shivam Singh <shivamcena07@gmail.com>
2020-08-21 01:56:57 +0530

    API Error Handling

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,13 +1,19 @@
+using System.Linq;
+using API.Errors;
+using API.Extensions;
 using API.Helpers;
+using API.Middleware;
 using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -25,20 +31,20 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            
             services.AddAutoMapper(typeof(MappingProfile));
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
-            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-        }
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+        }                        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
@@ -47,6 +53,9 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseSwaggerDocumentation();
+
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
