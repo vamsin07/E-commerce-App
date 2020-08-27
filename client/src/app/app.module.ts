3f8f403cdc9d52e5ad3810374bc408da7a9237b7
Shivam Singh <shivamcena07@gmail.com>
2020-08-28 00:28:20 +0530

    Pagination

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,20 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
+    ShopModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core';
+import { ShopService } from './shop.service';
+import { IProduct } from '../shared/models/product';
+import { IBrand } from '../shared/models/brand';
+import { IType } from '../shared/models/productType';
+import { ShopParams } from '../shared/models/shopParams';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss'],
+})
+export class ShopComponent implements OnInit {
+  products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  shopParams = new ShopParams();
+  totalCount: number;
+
+  sortOptions = [
+    { name: 'Alphabetical', value: 'name' },
+    { name: 'Price: Low To High', value: 'priceAsc' },
+    { name: 'Price: High To Low', value: 'priceDesc' },
+  ];
+
+  constructor(private shopService: ShopService) {}
+
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  // tslint:disable-next-line: typedef
+  getProducts() {
+    this.shopService.getProducts(this.shopParams).subscribe(
+      (response) => {
+        this.products = response.data;
+        this.shopParams.pageNumber = response.pageIndex;
+        this.shopParams.pageSize = response.pageSize;
+        this.totalCount = response.count;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  // tslint:disable-next-line: typedef
+  getBrands() {
+    this.shopService.getBrands().subscribe(
+      (response) => {
+        this.brands = [{ id: 0, name: 'All' }, ...response];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  // tslint:disable-next-line: typedef
+  getTypes() {
+    this.shopService.getTypes().subscribe(
+      (response) => {
+        this.types = [{ id: 0, name: 'All' }, ...response];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  // tslint:disable-next-line: typedef
+  onBrandSelected(brandId: number) {
+    this.shopParams.brandId = brandId;
+    this.getProducts();
+  }
+
+  // tslint:disable-next-line: typedef
+  onTypeSelected(typeId: number) {
+    this.shopParams.typeId = typeId;
+    this.getProducts();
+  }
+
+  // tslint:disable-next-line: typedef
+  onSortSelected(sort: string) {
+    this.shopParams.sort = sort;
+    this.getProducts();
+  }
+
+  // tslint:disable-next-line: typedef
+  onPageChanged(event: any) {
+    this.shopParams.pageNumber = event.page;
+    this.getProducts();
+  }
+}
