47d9ef0d040304482365c31a556b2cc972e64da1
Shivam Singh <shivamcena07@gmail.com>
2020-09-07 22:17:55 +0530

    Docker And Redis

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -7,36 +7,37 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Extensions
 {
-    public static class ApplicationServicesExtensions
+  public static class ApplicationServicesExtensions
+  {
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services )
-        {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+      services.AddScoped<IProductRepository, ProductRepository>();
+      services.AddScoped<IBasketRepository, BasketRepository>();
+      services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
 
-            services.Configure<ApiBehaviorOptions>(options => 
+      services.Configure<ApiBehaviorOptions>(options =>
+      {
+        options.InvalidModelStateResponseFactory = actionContext =>
+              {
+            var errors = actionContext.ModelState
+                     .Where(e => e.Value.Errors.Count > 0)
+                     .SelectMany(x => x.Value.Errors)
+                     .Select(x => x.ErrorMessage).ToArray();
+
+            var errorResponse = new ApiValidationErrorResponse
             {
-                options.InvalidModelStateResponseFactory = actionContext =>
-                {
-                    var errors = actionContext.ModelState
-                       .Where(e => e.Value.Errors.Count > 0)
-                       .SelectMany(x => x.Value.Errors)
-                       .Select(x => x.ErrorMessage).ToArray();
+              Errors = errors
+            };
 
-                    var errorResponse = new ApiValidationErrorResponse   
-                    {
-                        Errors = errors
-                    };
-
-                    return new BadRequestObjectResult(errorResponse);
+            return new BadRequestObjectResult(errorResponse);
 
 
 
-                };
-            } );
+          };
+      });
 
-              return services;
+      return services;
 
-        }
     }
+  }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -14,60 +14,66 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
 
 namespace API
 {
   public class Startup
+  {
+    private readonly IConfiguration _config;
+    public Startup(IConfiguration config)
     {
-        private readonly IConfiguration _config;
-        public Startup(IConfiguration config)
-        {
-            _config = config;
-        
-        }
+      _config = config;
 
-    
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            
-            services.AddAutoMapper(typeof(MappingProfile));
-            services.AddControllers();
-            services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            services.AddApplicationServices();
-            services.AddSwaggerDocumentation();
-            services.AddCors(opt => 
-            {
-               opt.AddPolicy("CorsPolicy", policy =>
-               {
-                  policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
-               });
-            });
-        }                        
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseMiddleware<ExceptionMiddleware>();
-
-            app.UseStatusCodePagesWithReExecute("/errors/{0}");
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-            app.UseStaticFiles();
-            app.UseCors("CorsPolicy");
-            
-            app.UseAuthorization();
-
-            app.UseSwaggerDocumentation();
-
-            
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
     }
+
+
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+      services.AddAutoMapper(typeof(MappingProfile));
+      services.AddControllers();
+      services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+      services.AddSingleton<IConnectionMultiplexer>(c =>
+      {
+        var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
+        return ConnectionMultiplexer.Connect(configuration);
+      });
+      services.AddApplicationServices();
+      services.AddSwaggerDocumentation();
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+              {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+              });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseMiddleware<ExceptionMiddleware>();
+
+      app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+      app.UseStaticFiles();
+      app.UseCors("CorsPolicy");
+
+      app.UseAuthorization();
+
+      app.UseSwaggerDocumentation();
+
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
